f0d14466de1e1eb4fd95e1f0149fb02c39039b41
Kotik-mur-kotik <Hacta14091993@gmail.com>
2020-08-28 15:45:07 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace laba3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int> { { 1, 10 }, { 2, 100 }, { 3, 25 }, { 4, 25 }, { 5, 125 } };
+            ArrayList list = new ArrayList();
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            foreach (var key in dictionary.Keys)
+            {
+                max = (max < dictionary[key]) ? dictionary[key] : max;
+                min = (min > dictionary[key]) ? dictionary[key] : min;
+            }
+            list.Add(max);
+            list.Add(min);
+            Console.WriteLine($"{list[0]}  {list[1]}");
+        }
+    }
+}
